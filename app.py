60884026ce0da237621aae68b98fd2b9b7edd77c
Archisman Ray <95351969+Mochoye@users.noreply.github.com>
2023-08-23 19:54:53 +0530

    Linked html files with the database

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request
+import sqlite3
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/attendance', methods=['POST'])
+def attendance():
+    selected_date = request.form.get('selected_date')
+    selected_date_obj = datetime.strptime(selected_date, '%Y-%m-%d')
+    formatted_date = selected_date_obj.strftime('%Y-%m-%d')
+
+    conn = sqlite3.connect('attendance.db')
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT name, time FROM attendance WHERE date = ?", (formatted_date,))
+    attendance_data = cursor.fetchall()
+
+    conn.close()
+
+    return render_template('attendance.html', selected_date=formatted_date, attendance_data=attendance_data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
